4b58947a93521e8c5356364a29ac99ec34099da4
Sam Abdelfattah <samfatth@gmail.com>
2017-08-18 16:22:29 -0600

    menu works

--- lunch_lady.rb
+++ lunch_lady.rb
@@ -1,10 +1,36 @@
 require 'pry'
 
-puts "Welcome to lunch!"
-puts "What you like for your main dish?"
-puts "1) Pizza"
-puts "2) Chicken Tenders"
-puts "3) Knuckle Sandwich"
+cart = []
+
+  puts "Welcome to lunch!"
+def main
+  puts "What you like for your main dish?"
+  puts "1) Pizza"
+  puts "2) Chicken Tenders"
+  puts "3) Knuckle Sandwich"
+end
+
+main
+
+choice_1 = gets.strip
+
+case
+  when choice_1 == '1'
+    puts "Pizza has been added to your cart."
+    cart << choice_1
+  when choice_1 == '2'
+    puts "Chicken tenders have been added to your cart."
+    cart << choice_1
+  when choice_1 == '3'
+    puts "A knuckle sandwich has been added to your cart."
+    cart << choice_1
+  when choice_1 == 'quit'
+    exit
+# else
+#   puts "Sorry, that is not an option."
+#   main
+end
+
 
 main_dish = {
   pizza: {
@@ -21,6 +47,34 @@ main_dish = {
   },
 }
 
+  puts "What do you want for your first side dish?"
+def side_1
+  puts "1) Fries"
+  puts "2) Steamed Veggies"
+  puts "3) Chips"
+end
+
+side_1
+
+choice_2 = gets.strip
+
+case
+  when choice_2 == '1'
+    puts "Fries have been added to your cart."
+    cart << choice_2
+  when choice_2 == '2'
+    puts "Steamed veggies have been added to your cart."
+    cart << choice_2
+  when choice_2 == '3'
+    puts "Chips have been added to your cart."
+    cart << choice_2
+  when choice_2 == 'quit'
+    exit
+# else
+#   puts "Sorry, that is not an option."
+#   main
+end
+
 side_dish = {
   fries: {
     price: 1.50,
@@ -35,3 +89,33 @@ side_dish = {
     calories: 180,
   },
 }
+
+def side_2
+  puts "What do you want for your second side dish?"
+  puts "1) Fries"
+  puts "2) Steamed Veggies"
+  puts "3) Chips"
+end
+
+side_2
+
+choice_3 = gets.strip
+
+case
+  when choice_3 == '1'
+    puts "Fries have been added to your cart."
+    cart << choice_3
+  when choice_3 == '2'
+    puts "Steamed veggies have been added to your cart."
+    cart << choice_3
+  when choice_3 == '3'
+    puts "Chips have been added to your cart."
+    cart << choice_3
+  when choice_3 == 'quit'
+    exit
+# else
+#   puts "Sorry, that is not an option."
+#   main
+end
+
+
