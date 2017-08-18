3117860bc3d1bae8f01e7a09806f73fd9dee270b
Sam Abdelfattah <samfatth@gmail.com>
2017-08-18 16:36:55 -0600

    closer

--- lunch_lady.rb
+++ lunch_lady.rb
@@ -1,5 +1,23 @@
 require 'pry'
 
+main_dish = {
+  pizza: {
+    name: 'Pizza',
+    price: '$3.99',
+    calories: 355,
+  },
+  chicken_tenders: {
+    name: 'Chicken Tenders',
+    price: '$3.49',
+    calories: 415,
+  },
+  kunckle_sandwich: {
+    name: 'Knuckle Sandwich',
+    price: '$1.19',
+    calories: 1,
+  },
+}
+
 cart = []
 
   puts "Welcome to lunch!"
@@ -16,36 +34,23 @@ choice_1 = gets.strip
 
 case
   when choice_1 == '1'
-    puts "Pizza has been added to your cart."
-    cart << choice_1
+    puts "Pizza for #{main_dish[:pizza][:price]} has been added to your cart."
+    cart << main_dish.[:pizza]
   when choice_1 == '2'
-    puts "Chicken tenders have been added to your cart."
+    puts "Chicken tenders for #{main_dish[:chicken_tenders][:price]} have been added to your cart."
     cart << choice_1
   when choice_1 == '3'
-    puts "A knuckle sandwich has been added to your cart."
+    puts "A knuckle sandwich for #{main_dish[:knuckle_sandwich][:price]} has been added to your cart."
     cart << choice_1
   when choice_1 == 'quit'
     exit
-# else
-#   puts "Sorry, that is not an option."
-#   main
+  else
+    puts "Sorry, that is not an option."
+    main
 end
 
 
-main_dish = {
-  pizza: {
-    price: 3.00,
-    calories: 300,
-  },
-  chicken_tenders: {
-    price: 3.50,
-    calories: 410,
-  },
-  kunckle_sandwich: {
-    price: 1.00,
-    calories: 10,
-  },
-}
+
 
   puts "What do you want for your first side dish?"
 def side_1
@@ -70,22 +75,22 @@ case
     cart << choice_2
   when choice_2 == 'quit'
     exit
-# else
-#   puts "Sorry, that is not an option."
-#   main
+  else
+    puts "Sorry, that is not an option."
+    main
 end
 
 side_dish = {
   fries: {
-    price: 1.50,
+    price: '$1.50',
   calories: 200,
   },
   steamed_veggies: {
-    price: 1.00,
+    price: '$1.00',
     calories: 80,
   },
   chips:{
-    price: 1.25,
+    price: '$1.25',
     calories: 180,
   },
 }
@@ -113,9 +118,10 @@ case
     cart << choice_3
   when choice_3 == 'quit'
     exit
-# else
-#   puts "Sorry, that is not an option."
-#   main
+  else
+    puts "Sorry, that is not an option."
+    main
 end
 
+puts cart
 
