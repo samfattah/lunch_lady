4af036b1685964c71cc60cdd114c1130afb0aa1e
Sam Abdelfattah <samfatth@gmail.com>
2017-08-18 15:55:31 -0600

    menu hash

--- lunch_lady.rb
+++ lunch_lady.rb
@@ -3,5 +3,20 @@ require 'pry'
 puts "Welcome to lunch!"
 puts "What you like for your main dish?"
 puts "1) Pizza"
-puts "2) Chicken Tenders"`
-puts "3) "
+puts "2) Chicken Tenders"
+puts "3) Knuckle Sandwich"
+
+main_dish = {
+  pizza: {
+    price: 3.00,
+    calories: 300,
+  },
+  chicken_tenders: {
+    price: 3.50,
+    calories: 410,
+  },
+  kunckle_sandwich: {
+    price: 1.00,
+    calories: 10,
+  },
+}
